095810cc32961742a3fbe072b8d6338561889cb8
152056126 <1776342737@qq.com>
2018-01-10 12:58:33 +0800

    first

---
+++ version1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace version1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Note N1 = new Note();
+            N1.clasifyNote();
+            N1.openNote();
+        }
+    }
+}
